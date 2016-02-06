87d7228fdfb1812aa4a7cadd4d5fc742c06c1958
Timon Loeffen <timon.loeffen@gmail.com>
2016-02-06 18:40:07 +0100

    extract findFiles

--- src/main/java/FileUtils.java
+++ src/main/java/FileUtils.java
@@ -1,6 +1,13 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.util.AntPathMatcher;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
@@ -17,5 +24,40 @@ public class FileUtils {
          final List<String> lines) throws IOException {
       Files.write(Joiner.on("\n").join(lines), new File(fileName),
             Charsets.UTF_8);
+   }
+
+   public static List<String> findFiles(final File parent,
+         final List<String> antPatterns) throws IOException {
+      final Path parentPath = parent.toPath();
+      final List<String> files = new ArrayList<>();
+      java.nio.file.Files.walkFileTree(parentPath,
+            new PathMatchingVisitor(parentPath, files, antPatterns));
+      return files;
+   }
+
+   private static final class PathMatchingVisitor
+         extends SimpleFileVisitor<Path> {
+      private final Path parentPath;
+      private final List<String> files;
+      private final List<String> antPatterns;
+      private final AntPathMatcher matcher = new AntPathMatcher();
+
+      private PathMatchingVisitor(final Path parentPath,
+            final List<String> files, final List<String> antPatterns) {
+         this.parentPath = parentPath;
+         this.files = files;
+         this.antPatterns = antPatterns;
+      }
+
+      @Override
+      public FileVisitResult visitFile(final Path file,
+            final BasicFileAttributes attrs) throws IOException {
+         final String relativePath = parentPath.relativize(file).toString();
+         if(antPatterns.stream()
+               .anyMatch(pattern -> matcher.match(pattern, relativePath))) {
+            files.add(file.toString());
+         }
+         return super.visitFile(file, attrs);
+      }
    }
 }
--- src/main/java/GeneratorMojo.java
+++ src/main/java/GeneratorMojo.java
@@ -1,10 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,7 +11,6 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.springframework.util.AntPathMatcher;
 
 @Mojo(name = "generateGetters", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class GeneratorMojo extends AbstractMojo {
@@ -42,50 +36,28 @@ public class GeneratorMojo extends AbstractMojo {
       maker = new GetterMaker(suffix == null ? "" : suffix);
       final Log log = getLog();
       log.info("generating getters");
-      if(includes == null) {
+      if(includes == null || includes.isEmpty()) {
          includes = new ArrayList<>();
-      }
-      if(includes.isEmpty()) {
          includes.add("**/*.java");
       }
       log.debug(includes.toString());
       log.debug(directory.toString());
       final List<String> files = getFiles(log);
-      files.forEach(this::analyzeFileAndReplaceOrPrintGetters);
+      files.forEach(this::analyzeFileAndReplaceGetters);
    }
 
    private List<String> getFiles(final Log log) {
-      final List<String> files = new ArrayList<>();
+      final List<String> files;
       try {
-
-         final Path directoryAsPath = directory.toPath();
-         Files.walkFileTree(directoryAsPath, new SimpleFileVisitor<Path>() {
-            private final AntPathMatcher matcher = new AntPathMatcher();
-
-            @Override
-            public FileVisitResult visitFile(final Path file,
-                  final BasicFileAttributes attrs) throws IOException {
-               final String relativePath =
-                     directoryAsPath.relativize(file).toString();
-               log.debug("matching " + relativePath);
-               if(includes.stream().anyMatch(pattern -> {
-                  return matcher.match(pattern, relativePath);
-               })) {
-                  log.debug("adding " + relativePath);
-                  files.add(file.toString());
-               }
-               return super.visitFile(file, attrs);
-            }
-         });
+         files = FileUtils.findFiles(directory, includes);
       } catch(final IOException e) {
          throw new RuntimeException(e);
       }
-
-      log.info("will process " + files.size() + " files");
+      log.info("will process " + files.size() + " file(s)");
       return files;
    }
 
-   private void analyzeFileAndReplaceOrPrintGetters(final String file) {
+   private void analyzeFileAndReplaceGetters(final String file) {
       final String getters =
             getters(file).stream().collect(Collectors.joining(delimiter));
       replacer.replaceIn(file, getters);
